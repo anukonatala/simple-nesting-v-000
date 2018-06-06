a79d498f3444f459cd40368f206047dd6f3dccee
Anusha Konatala <anu.konatala@gmail.com>
2018-06-06 00:10:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -111,8 +111,11 @@ def changing_alan
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
      
+     puts "#{programmer_hash[:alan_kay][:known_for]}"
      
 end
+
+changing_alan
 
 def adding_to_dennis
 	programmer_hash = 
